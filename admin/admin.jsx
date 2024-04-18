4f40e68369270a7a5e5f882ac13e812d229dc38a
Timofey Fedorov <tramvai00105@gmail.com>
2024-04-18 12:53:30 +0300

    only prod files

--- admin/admin.jsx
+++
@@ -1,7 +0,0 @@
-import './admin.css'
-import React from 'react'
-import { createRoot } from 'react-dom/client';
-import AdminPanel from "./AdminPanel.tsx"
-
-const root = createRoot(document.getElementById('root'));
-root.render(<AdminPanel />);

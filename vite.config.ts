8ecb055bb027c39f8d7bd68df0c88feda68b86ad
Timofey Fedorov <tramvai00105@gmail.com>
2024-04-28 11:46:55 +0300

    build 0.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(resolve(), 'index.html'),
+        admin: resolve(resolve(), 'admin/index.html'),
+      },
+    },
+  },
 })

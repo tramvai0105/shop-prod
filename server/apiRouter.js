69880ca480548bf61915cc1b16f4d57cc0c4b07f
Timofey Fedorov <tramvai00105@gmail.com>
2024-04-28 11:50:08 +0300

    b 0.1.0 server from main

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import { Product } from './models.js'
 import inspector from "schema-inspector";
-const apiRouter = express.Router()
+const apiRouter = express.Router();
+import multer from "multer";
+const upload = multer({ dest: "uploads/" });
 
 // middleware that is specific to this router
 const middleware = (req, res, next) => {
@@ -20,7 +22,35 @@ apiRouter.get('/products', async (req, res) => {
   res.send(JSON.stringify(products))
 })
 
-apiRouter.post('/postProduct', async (req, res) => {
+apiRouter.post('/putProduct', upload.array("files"), async (req, res) => {
+  let validation = {
+    type: 'object',
+    properties: {
+      _id: {type: "string", minLength: 1},
+      name: { type: 'string', minLength: 5 },
+      category: { type: 'string', minLength: 5 },
+      price: { type: 'number', minLength: 1 },
+      description: { type: 'string', minLength: 5 },
+    },
+  };
+  let validated = inspector.validate(validation, req.body)
+  if(!validated.valid){
+    return res.status(501).json({message: "Неверные данные"});
+  }
+  if(req.files && req.files[0] 
+    && (req.files[0].mimetype != "image/png"
+      || req.files[0].mimetype != "image/jpg")){
+    return res.status(501).json({message: "Неверный тип файла"});
+  }
+  try{
+    await Product.findOneAndUpdate({_id: req.body._id}, req.body)
+    return res.status(200).json({message: "Данные обновлены"});
+  }catch(error){
+    return res.status(501).json({message: error});
+  }
+})
+
+apiRouter.post('/postProduct', upload.array("files"), async (req, res) => {
   let validation = {
     type: 'object',
     properties: {
--- server/authRouter.js
+++ server/authRouter.js
@@ -11,26 +11,52 @@ const middleware = (req, res, next) => {
 
 authRouter.use(middleware)
 
-authRouter.get('/', (req, res) => {
-    res.json(200)
+authRouter.get('/checktoken', (req, res) => {
+    let token = req.cookies["token"];
+    if (!token) {
+        return res.status(200).json({message: false})
+    }
+    try {
+        let payload = jwt.verify(token, process.env.SECRET)
+        return res.status(200).send({message: {username: payload.username, privileges: payload.privileges}});
+    } catch (error) {
+        return res.status(200).send({message: false});
+    }
 })
 
 authRouter.get('/registerkingpiglsey', async (req, res) => {
     let password = bcrypt.hashSync("strongpassword", 10);
-    let user = new User({username: "admin", password: password,
-    privileges: true});
+    let user = new User({
+        username: "admin", password: password,
+        privileges: true
+    });
     await user.save();
     res.json(200)
 })
 
-authRouter.get('/login', async (req, res) => {
-    let user = await User.findOne({ username: "admin" })
-    let correct = await bcrypt.compare("strongpassword", user.password)
-    if(!correct){
-        res.status(401).send("incorrect password")
+authRouter.post('/login', async (req, res) => {
+    let validation = {
+        type: 'object',
+        properties: {
+            username: { type: 'string', minLength: 1 },
+            password: { type: 'string', minLength: 1 },
+        },
+    };
+    let validated = inspector.validate(validation, req.body)
+    console.log(req.body)
+    if (!validated.valid) {
+        return res.status(501).json({ message: "Некорректные данные" });
     }
-    let token = jwt.sign(user.toJSON(), process.env.SECRET, { expiresIn: 1200 });
-    res.cookie("token", token).status(200).send("authorized")
+    let user = await User.findOne({ username: req.body.username })
+    if (!user) {
+        return res.status(501).json({ message: "Пользователь не найден" });
+    }
+    let correct = await bcrypt.compare(req.body.password, user.password)
+    if (!correct) {
+        return res.status(401).json({ message: "Неправильный пароль" })
+    }
+    let token = jwt.sign(user.toJSON(), process.env.SECRET, { expiresIn: 12000 });
+    return res.cookie("token", token).status(200).json({ message: "Успешно авторизировано" })
 })
 
 export default authRouter

98ddf04dd8cb56eb003e058ab4b28278daf8bd23
Timofey Fedorov <tramvai00105@gmail.com>
2024-05-06 13:29:28 +0300

    added files serve static to server

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static('public'))
+app.use(express.static('files'))
 
 // Add Vite or respective production middlewares
 let vite
